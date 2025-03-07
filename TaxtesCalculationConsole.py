c3c5a250dd64a4d5c1d0851c5a45f1c341f82b3e
SantiResFonn <159478006+SantiResFonn@users.noreply.github.com>
2025-03-06 20:41:16 -0500

    Limpieza de más codigo

--- TaxtesCalculationConsole.py
+++ TaxtesCalculationConsole.py
@@ -4,15 +4,15 @@ import BuyLogic
 try:
         valor_compra=float(input("Ingrese el valor de la compra: "))
         valor_iva=float(input("Ingrese el porcentaje del IVA aplicable a la compra: "))
-        valor_ic=float(input("Ingrese el porcentaje del Impuesto al consumo aplicable a la compra: "))
+        valor_impuesto_consumo=float(input("Ingrese el porcentaje del Impuesto al consumo aplicable a la compra: "))
         impuesto_licor=float(input("Ingrese el porcentaje del Impuesto al licor aplicable a la compra: "))
         impuesto_bolsa=input("¿A su compra se le aplica impuesto de bolsa?(Si/No)")
 
         #Proceso
-        total = BuyLogic.calcular_compra(valor_compra,valor_iva,valor_ic,impuesto_licor,impuesto_bolsa)
+        total = BuyLogic.calcular_total_compra(valor_compra,valor_iva,valor_ic,impuesto_licor,impuesto_bolsa)
 
         #Salidas
 
         print(f"El valor total de la compra es {total}")
 except Exception as ex:
-    print(f"ALERTA: Ocurrio un Error {ex}")
+        print(f"ALERTA: Ocurrio un Error {ex}")
